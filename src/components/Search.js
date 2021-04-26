2c7287d95458e3d5164d87405e0141510078367e
Ben T <ben.trevethan@gmail.com>
2021-04-26 21:01:19 +0930

    prevent default on search and update state on search string

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,44 @@
 import React from 'react'
 
 class Search extends React.Component {
+    state = {
+        searchString: "",
+    };
+
+    handleInputChange = event => {
+        let value = event.target.value;
+        const name = event.target.name;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
     render() {
         return (
             <div>
-                <form className="form-inline justify-content-center">
-                    <input type="text" placeholder="Search" className="form-control">
-                    </input>
+                <form 
+                    onSubmit={this.handleFormSubmit}
+                    onChange={this.handleInputChange}
+                    className="form-inline justify-content-center">
+                    <input 
+                        type="text"
+                        name="searchString"
+                        value={this.state.searchString} 
+                        placeholder="Search" 
+                        className="form-control"
+                        onChange={this.handleInputChange}
+                        onSubmit={this.handleFormSubmit}
+                        />
                 </form>
                 <br></br>
             </div>
         )
     }
-}
+};
 
 export default Search;
