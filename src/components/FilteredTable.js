ad15a19459e2d308d908f092ea9aaa660b948047
Ben T <ben.trevethan@gmail.com>
2021-05-02 15:52:48 +0930

    filtering udpated to render on a ternary without breaking the page

--- src/components/FilteredTable.js
+++ src/components/FilteredTable.js
@@ -1,17 +1,30 @@
 import React from "react";
 
 function FilteredTable(props) {
-    const filtered = props.employees.filter(employee => employee.name.first.toLowerCase().includes(props.search))
-    console.log(filtered)
-        return (
-            <tr>
-            <th scope="row">Sorted</th>
-            <td>5</td>
-            <td>4</td>
-            <td>3</td>
-            </tr>
-        )
-    
+
+  const filtered = props.employees.filter(
+    (employee) =>
+      employee.name.first.toLowerCase().includes(props.search) ||
+      employee.name.last.toLowerCase().includes(props.search)
+    );
+
+    return (
+        <>
+        {filtered.length > 0 ? 
+        <tr>
+        <th scope="row">Sorted</th>
+        <td>{filtered[0].name.first}</td>
+        <td>4</td>
+        <td>5</td>
+        </tr>
+        :
+        <tr>
+            <th scope="row"></th>
+            <td>No matches found</td>
+        </tr>
+        }
+        </>        
+    );
 }
 
 export default FilteredTable;
